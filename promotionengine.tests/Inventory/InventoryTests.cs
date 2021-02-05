77f7b6617f90b41f443809d94c36cb3e906f9e75
Harsh Kumar <harsh.nitk@gmail.com>
2021-02-05 14:20:39 +0100

    Unit tests for inventory

---
+++ promotionengine.tests/Inventory/InventoryTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace promotionengine.tests.Inventory
+{
+    public class InventoryTests
+    {
+        private readonly IInventoryService _inventoryService;
+        public InventoryTests()
+        {
+            this._inventoryService = new InventoryService();
+        }
+
+        [Theory]
+        [InlineData("A", 20, "$")]
+        [InlineData("B", 30, "$")]
+        [InlineData("C", 40, "$")]
+        [InlineData("D", 40, "€")]
+        public void SKUCreation_Test (string id, int price, string currency)
+        {
+            Assert.True(this._inventoryService.CreateSKU(id, price, currency), "SKU " + id + " created");
+        }
+
+    }
+}
